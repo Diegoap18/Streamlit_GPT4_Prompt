1fc8c6020aa284dbd4a780e20ef726de6c23ac55
Diego A Pereira <56772594+Diegoap18@users.noreply.github.com>
2023-05-18 15:09:01 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,58 @@
+# Importing required packages
+import streamlit as st
+import openai
+import time
+time.sleep(0.100)
+
+
+st.title("Streamlit Prompt to ChatGPT-4")
+st.sidebar.header("Instructions")
+st.sidebar.info(
+    '''This is a web application that allows you to interact with 
+       the OpenAI API's implementation of the ChatGPT model.
+       Enter a **query** in the **text box** and **press enter** to receive 
+       a **response** from the ChatGPT
+       '''
+    )
+   
+ # Set the model engine and your OpenAI API key
+model_engine = "text-davinci-003"
+openai.api_key = "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" #follow step 4 to get a secret_key
+
+
+def main():
+    '''
+    This function gets the user input, pass it to ChatGPT function and 
+    displays the response
+    '''
+    response ='1'
+    # Get user input
+    user_query = st.text_input("Enter query here, to exit enter :q", "")
+    time.sleep(5)
+    if user_query != ":q" or user_query != "":
+        # Pass the query to the ChatGPT function
+        response = ChatGPT(user_query)
+        
+        return st.markdown(response, unsafe_allow_html=True)
+        
+def ChatGPT(user_query):
+       ''' 
+    This function uses the OpenAI API to generate a response to the given 
+    user_query using the ChatGPT model
+    '''
+    # Use the OpenAI API to generate a response
+    completion = openai.Completion.create(
+                                  engine = model_engine,
+                                  prompt = user_query,
+                                  max_tokens = 1024,
+                                  n = 1,
+                                  temperature = 0.5,
+                                      )
+    response = completion.choices[0].text
+    
+    
+    return response
+    
+ 
+ # call the main function
+main() 
